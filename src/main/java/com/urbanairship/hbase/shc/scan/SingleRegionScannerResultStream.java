65e3703a2a9772c3a3ece408e3bc6093e47be27a
Scott Hirdes <scotth@urbanairship.com>
2013-10-21 21:11:08 -0700

    simplify

--- src/main/java/com/urbanairship/hbase/shc/scan/SingleRegionScannerResultStream.java
+++ src/main/java/com/urbanairship/hbase/shc/scan/SingleRegionScannerResultStream.java
@@ -1,10 +1,9 @@
 package com.urbanairship.hbase.shc.scan;
 
-import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Supplier;
 import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.Lists;
-import org.apache.hadoop.hbase.HRegionLocation;
 import org.apache.hadoop.hbase.client.Result;
 
 import java.io.Closeable;
@@ -13,43 +12,27 @@ import java.util.Queue;
 
 public class SingleRegionScannerResultStream extends AbstractIterator<Result> implements ScannerResultStream {
 
-    private final HRegionLocation location;
-    private final Function<HRegionLocation, Long> scannerOpener;
-    private final Function<Long, ScannerBatchResult> batchLoader;
+    private final Supplier<ScannerBatchResult> batchLoader;
     private final Closeable closeFunction;
-
-    private boolean open;
-    private long currentScannerId;
 
     private Queue<Result> currentResults;
 
     private ScannerBatchResult.Status finalStatus;
 
-    public SingleRegionScannerResultStream(HRegionLocation location,
-                                           Function<HRegionLocation, Long> scannerOpener,
-                                           Function<Long, ScannerBatchResult> batchLoader,
-                                           Closeable closeFunction) {
-        this.location = location;
-        this.scannerOpener = scannerOpener;
+    public SingleRegionScannerResultStream(Supplier<ScannerBatchResult> batchLoader, Closeable closeFunction) {
         this.batchLoader = batchLoader;
         this.closeFunction = closeFunction;
 
-        this.open = false;
         this.currentResults = Lists.newLinkedList();
     }
 
     @Override
     protected Result computeNext() {
-        if (!open) {
-            currentScannerId = scannerOpener.apply(location);
-        }
-
         if (!currentResults.isEmpty()) {
             return currentResults.remove();
         }
 
-        // TODO: how do we handle the possibility that we might need to open a new scanner?
-        ScannerBatchResult nextBatch = batchLoader.apply(currentScannerId);
+        ScannerBatchResult nextBatch = batchLoader.get();
         if (nextBatch.getStatus() != ScannerBatchResult.Status.RESULTS_AVAILABLE) {
             finalStatus = nextBatch.getStatus();
             return endOfData();
